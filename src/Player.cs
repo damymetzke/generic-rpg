93b7766fbaea197e85052b77da3ce614a8f99324
Damy Metzke <damy.metzke@gmail.com>
2020-10-25 17:05:31 +0100

    Improve player movement.
    Use action strength instead of boolean is action pressed..

--- src/Player.cs
+++ src/Player.cs
@@ -3,124 +3,103 @@ using System;
 
 public class Player : KinematicBody2D
 {
-	private AnimatedSprite animatedSprite;
+    private AnimatedSprite animatedSprite;
 
-	private enum HorizontalDirection
-	{
-		LEFT,
-		NONE,
-		RIGHT
-	}
-	private enum VerticalDirection
-	{
-		UP,
-		NONE,
-		DOWN
-	}
+    private enum HorizontalDirection
+    {
+        LEFT,
+        NONE,
+        RIGHT
+    }
+    private enum VerticalDirection
+    {
+        UP,
+        NONE,
+        DOWN
+    }
 
-	[Export]
-	private float movementSpeed = 20.0f;
+    [Export]
+    private float movementSpeed = 20.0f;
 
-	private Vector2 motion;
+    private Vector2 motion;
 
-	public override void _Ready()
-	{
-		base._Ready();
-		animatedSprite = GetNode<AnimatedSprite>("CharacterSprite");
-		animatedSprite.Play("idle");
-	}
+    public override void _Ready()
+    {
+        base._Ready();
+        animatedSprite = GetNode<AnimatedSprite>("CharacterSprite");
+        animatedSprite.Play("idle");
+    }
 
-	public override void _PhysicsProcess(float delta)
-	{
+    public override void _PhysicsProcess(float delta)
+    {
 
-		bool walkingLeft = Input.IsActionPressed("player_left");
-		bool walkingRight = Input.IsActionPressed("player_right");
+        bool walkingLeft = Input.IsActionPressed("player_left");
+        bool walkingRight = Input.IsActionPressed("player_right");
 
-		bool walkingUp = Input.IsActionPressed("player_up");
-		bool walkingDown = Input.IsActionPressed("player_down");
+        bool walkingUp = Input.IsActionPressed("player_up");
+        bool walkingDown = Input.IsActionPressed("player_down");
 
-		HorizontalDirection horizontalDirection = (walkingLeft == walkingRight)
-			? HorizontalDirection.NONE
-			: (walkingLeft) ? HorizontalDirection.LEFT : HorizontalDirection.RIGHT;
+        HorizontalDirection horizontalDirection = (walkingLeft == walkingRight)
+            ? HorizontalDirection.NONE
+            : (walkingLeft) ? HorizontalDirection.LEFT : HorizontalDirection.RIGHT;
 
-		VerticalDirection verticalDirection = (walkingUp == walkingDown)
-			? VerticalDirection.NONE
-			: (walkingUp) ? VerticalDirection.UP : VerticalDirection.DOWN;
+        VerticalDirection verticalDirection = (walkingUp == walkingDown)
+            ? VerticalDirection.NONE
+            : (walkingUp) ? VerticalDirection.UP : VerticalDirection.DOWN;
 
-		Vector2 direction = new Vector2(0.0f, 0.0f);
+        if (horizontalDirection == HorizontalDirection.RIGHT)
+        {
+            animatedSprite.Play("walk-right");
+        }
+        else if (horizontalDirection == HorizontalDirection.LEFT)
+        {
+            animatedSprite.Play("walk-left");
+        }
+        else if (verticalDirection == VerticalDirection.DOWN)
+        {
+            animatedSprite.Play("walk-down");
+        }
+        else if (verticalDirection == VerticalDirection.UP)
+        {
+            animatedSprite.Play("walk-up");
+        }
+        else
+        {
+            animatedSprite.Play("idle");
+        }
 
-		switch (horizontalDirection)
-		{
-			case HorizontalDirection.LEFT:
-				direction.x = -1.0f;
-				break;
-			case HorizontalDirection.NONE:
-				direction.x = 0.0f;
-				break;
-			case HorizontalDirection.RIGHT:
-				direction.x = 1.0f;
-				break;
-		}
+        Vector2 direction = new Vector2(
+            Input.GetActionStrength("player_right") - Input.GetActionStrength("player_left"),
+            Input.GetActionStrength("player_down") - Input.GetActionStrength("player_up")
+        );
 
-		switch (verticalDirection)
-		{
-			case VerticalDirection.UP:
-				direction.y = -1.0f;
-				break;
-			case VerticalDirection.NONE:
-				direction.y = 0.0f;
-				break;
-			case VerticalDirection.DOWN:
-				direction.y = 1.0f;
-				break;
-		}
+        if (direction.Length() >= 1.0f)
+        {
+            direction = direction.Normalized();
+        }
 
-		if (horizontalDirection == HorizontalDirection.RIGHT)
-		{
-			animatedSprite.Play("walk-right");
-		}
-		else if (horizontalDirection == HorizontalDirection.LEFT)
-		{
-			animatedSprite.Play("walk-left");
-		}
-		else if (verticalDirection == VerticalDirection.DOWN)
-		{
-			animatedSprite.Play("walk-down");
-		}
-		else if (verticalDirection == VerticalDirection.UP)
-		{
-			animatedSprite.Play("walk-up");
-		}
-		else
-		{
-			animatedSprite.Play("idle");
-		}
+        float frameAcceleration = movementSpeed * 100.0f * delta;
 
-		float frameAcceleration = movementSpeed * 100.0f * delta;
+        if (
+            horizontalDirection == HorizontalDirection.NONE
+            && verticalDirection == VerticalDirection.NONE
+        )
+        {
+            if (motion.Length() > frameAcceleration)
+            {
+                motion -= motion.Normalized() * frameAcceleration;
+            }
+            else
+            {
+                motion = new Vector2();
+            }
+        }
+        else
+        {
+            motion += frameAcceleration * direction.Normalized();
+            motion = motion.Clamped(movementSpeed * 10.0f);
+        }
 
-		if (
-			horizontalDirection == HorizontalDirection.NONE
-			&& verticalDirection == VerticalDirection.NONE
-		)
-		{
-			if (motion.Length() > frameAcceleration)
-			{
-				motion -= motion.Normalized() * frameAcceleration;
-			}
-			else
-			{
-				motion = new Vector2();
-			}
-		}
-		else
-		{
-			motion += frameAcceleration * direction.Normalized();
-			motion = motion.Clamped(movementSpeed * 10.0f);
-		}
-
-		motion = MoveAndSlide(motion);
-
-		// this.LinearVelocity = delta * direction * 1000.0f * this.movementSpeed;
-		// this.Rotation = 0.0f;
-	}
+        motion = MoveAndSlide(motion);
+    }
 }

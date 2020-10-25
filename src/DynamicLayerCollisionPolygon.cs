d587085058772ed31ae66069f37201a967b08fe9
Damy Metzke <damy.metzke@gmail.com>
2020-10-25 15:15:49 +0100

    Implement DynamicLayerCollisionPolygon.
    Fix player hitbox.

--- src/DynamicLayerCollisionPolygon.cs
+++ src/DynamicLayerCollisionPolygon.cs
@@ -4,15 +4,32 @@ using System;
 public class DynamicLayerCollisionPolygon : CollisionPolygon2D
 {
 
-    public void OnSwitchLayer(byte newLayer)
-    {
-        GD.Print(newLayer);
-    }
-    public override void _Ready()
-    {
-        DynamicLayerSingleton dynamicLayer = (DynamicLayerSingleton)GetNode("/root/DynamicLayerSingleton");
-        dynamicLayer.BindOnSwitchLayer(this.OnSwitchLayer);
+	[Export]
+	public byte activateOn = 0;
 
-        dynamicLayer.SwitchLayer(5);
-    }
+	public void OnSwitchLayer(byte newLayer)
+	{
+		if (newLayer == activateOn)
+		{
+			this.Disabled = false;
+		}
+		else
+		{
+			this.Disabled = true;
+		}
+	}
+	public override void _Ready()
+	{
+		DynamicLayerSingleton dynamicLayer = (DynamicLayerSingleton)GetNode("/root/DynamicLayerSingleton");
+		dynamicLayer.BindOnSwitchLayer(this.OnSwitchLayer);
+
+		if (dynamicLayer.GetCurrentLayer() == activateOn)
+		{
+			this.Disabled = false;
+		}
+		else
+		{
+			this.Disabled = true;
+		}
+	}
 }

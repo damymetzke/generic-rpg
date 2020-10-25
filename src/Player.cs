fb8b4651da8f64383d371aa7d719cb1c8415faaa
Damy Metzke <damy.metzke@gmail.com>
2020-10-25 09:30:27 +0100

    Add walk animation for down.

--- src/Player.cs
+++ src/Player.cs
@@ -3,27 +3,86 @@ using System;
 
 public class Player : RigidBody2D
 {
+	private AnimatedSprite animatedSprite;
+
+	private enum HorizontalDirection
+	{
+		LEFT,
+		NONE,
+		RIGHT
+	}
+	private enum VerticalDirection
+	{
+		UP,
+		NONE,
+		DOWN
+	}
+
 	[Export]
 	private float movementSpeed = 20.0f;
+
+	public override void _Ready()
+	{
+		base._Ready();
+		animatedSprite = GetNode<AnimatedSprite>("CharacterSprite");
+		animatedSprite.Play("idle");
+	}
+
 	public override void _Process(float delta)
 	{
+
+		bool walkingLeft = Input.IsActionPressed("player_left");
+		bool walkingRight = Input.IsActionPressed("player_right");
+
+		bool walkingUp = Input.IsActionPressed("player_up");
+		bool walkingDown = Input.IsActionPressed("player_down");
+
+		HorizontalDirection horizontalDirection = (walkingLeft == walkingRight)
+			? HorizontalDirection.NONE
+			: (walkingLeft) ? HorizontalDirection.LEFT : HorizontalDirection.RIGHT;
+
+		VerticalDirection verticalDirection = (walkingUp == walkingDown)
+			? VerticalDirection.NONE
+			: (walkingUp) ? VerticalDirection.UP : VerticalDirection.DOWN;
+
 		Vector2 direction = new Vector2(0.0f, 0.0f);
 
-		if (Input.IsActionPressed("player_left"))
+		switch (horizontalDirection)
 		{
-			direction += new Vector2(-1.0f, 0.0f);
+			case HorizontalDirection.LEFT:
+				direction.x = -1.0f;
+				break;
+			case HorizontalDirection.NONE:
+				direction.x = 0.0f;
+				break;
+			case HorizontalDirection.RIGHT:
+				direction.x = 1.0f;
+				break;
 		}
-		if (Input.IsActionPressed("player_right"))
+
+		switch (verticalDirection)
 		{
-			direction += new Vector2(1.0f, 0.0f);
+			case VerticalDirection.UP:
+				direction.y = -1.0f;
+				break;
+			case VerticalDirection.NONE:
+				direction.y = 0.0f;
+				break;
+			case VerticalDirection.DOWN:
+				direction.y = 1.0f;
+				break;
 		}
-		if (Input.IsActionPressed("player_up"))
+
+		if (
+			horizontalDirection != HorizontalDirection.NONE
+			|| verticalDirection != VerticalDirection.NONE
+		)
 		{
-			direction += new Vector2(0.0f, -1.0f);
+			animatedSprite.Play("walk-down");
 		}
-		if (Input.IsActionPressed("player_down"))
+		else
 		{
-			direction += new Vector2(0.0f, 1.0f);
+			animatedSprite.Play("idle");
 		}
 
 

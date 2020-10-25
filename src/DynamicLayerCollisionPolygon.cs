8ff684e64b99fe93ed301c7388837c2165160a70
Damy Metzke <damy.metzke@gmail.com>
2020-10-25 16:54:00 +0100

    Remove debug print statement.

--- src/DynamicLayerCollisionPolygon.cs
+++ src/DynamicLayerCollisionPolygon.cs
@@ -4,33 +4,32 @@ using System;
 public class DynamicLayerCollisionPolygon : StaticBody2D
 {
 
-	[Export]
-	public byte activateOn = 0;
+    [Export]
+    public byte activateOn = 0;
 
-	public void OnSwitchLayer(byte newLayer)
-	{
-		GD.Print(newLayer);
-		if (newLayer == activateOn)
-		{
-			SetCollisionLayerBit(0, true);
-		}
-		else
-		{
-			SetCollisionLayerBit(0, false);
-		}
-	}
-	public override void _Ready()
-	{
-		DynamicLayerSingleton dynamicLayer = (DynamicLayerSingleton)GetNode("/root/DynamicLayerSingleton");
-		dynamicLayer.BindOnSwitchLayer(this.OnSwitchLayer);
+    public void OnSwitchLayer(byte newLayer)
+    {
+        if (newLayer == activateOn)
+        {
+            SetCollisionLayerBit(0, true);
+        }
+        else
+        {
+            SetCollisionLayerBit(0, false);
+        }
+    }
+    public override void _Ready()
+    {
+        DynamicLayerSingleton dynamicLayer = (DynamicLayerSingleton)GetNode("/root/DynamicLayerSingleton");
+        dynamicLayer.BindOnSwitchLayer(this.OnSwitchLayer);
 
-		if (dynamicLayer.GetCurrentLayer() == activateOn)
-		{
-			SetCollisionLayerBit(0, true);
-		}
-		else
-		{
-			SetCollisionLayerBit(0, false);
-		}
-	}
+        if (dynamicLayer.GetCurrentLayer() == activateOn)
+        {
+            SetCollisionLayerBit(0, true);
+        }
+        else
+        {
+            SetCollisionLayerBit(0, false);
+        }
+    }
 }

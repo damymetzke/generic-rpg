96857197f20943ef84f1fdb76b14f8b8b66b24b8
Damy Metzke <damy.metzke@gmail.com>
2020-10-26 17:10:47 +0100

    Add follow range to camera.

--- src/singleton/DynamicCameraSingleton.cs
+++ src/singleton/DynamicCameraSingleton.cs
@@ -3,11 +3,22 @@ using System;
 
 public class DynamicCameraSingleton : Node
 {
+    private float followRange = 130.0f;
+
     private Vector2 targetPosition;
 
     public void UpdateTarget(Vector2 desiredPosition)
     {
-        targetPosition = desiredPosition;
+        Vector2 nextTargetPosition = targetPosition;
+        if (Mathf.Abs(targetPosition.x - desiredPosition.x) >= followRange)
+        {
+            nextTargetPosition.x = desiredPosition.x + followRange * Mathf.Sign(targetPosition.x - desiredPosition.x);
+        }
+        if (Mathf.Abs(targetPosition.y - desiredPosition.y) >= followRange)
+        {
+            nextTargetPosition.y = desiredPosition.y + followRange * Mathf.Sign(targetPosition.y - desiredPosition.y);
+        }
+        targetPosition = nextTargetPosition;
     }
 
     public Vector2 GetTarget()

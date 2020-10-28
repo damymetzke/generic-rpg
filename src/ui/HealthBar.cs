7e0cd97bf7b8d6f95cf9ed07c819de0f2d4c19b7
Damy Metzke <damy.metzke@gmail.com>
2020-10-28 16:37:46 +0100

    Implement health text.

--- src/ui/HealthBar.cs
+++ src/ui/HealthBar.cs
@@ -4,12 +4,14 @@ using System;
 public class HealthBar : VBoxContainer
 {
     private TextureRect functionalBar;
+    private Label text;
 
     public override void _Ready()
     {
         base._Ready();
 
         functionalBar = (TextureRect)GetNode("Bar/FunctionalBar");
+        text = (Label)GetNode("Text");
     }
 
     public void UpdateHealthBar(uint currentHealth, uint maxHealth)
@@ -22,6 +24,8 @@ public class HealthBar : VBoxContainer
         }
 
         ((ShaderMaterial)functionalBar.Material).SetShaderParam("progress", Mathf.Clamp(progress, 0.0f, 1.0f));
+
+        text.Text = $"HP: {currentHealth}/{maxHealth}";
     }
 
 }

c8a743e6a035dd1f13c72c3c712ddbb727ebbb41
Damy Metzke <damy.metzke@gmail.com>
2020-10-30 09:07:35 +0100

    Implement ability circle.

--- shaders/GreyscaleTiledTexture.shader
+++ shaders/GreyscaleTiledTexture.shader
@@ -12,7 +12,7 @@ void vertex()
 
 void fragment()
 {
-    float tileOffset = mod(currentTile / numTiled,  1.0);
+    float tileOffset = mod(ceil(currentTile) / numTiled,  1.0);
     float localOffset = UV.x / numTiled;
     COLOR = texture(TEXTURE, vec2(tileOffset + localOffset, UV.y)) * colorModifier;
 }
--- src/ui/AbilityCircle.cs
+++ src/ui/AbilityCircle.cs
@@ -3,5 +3,21 @@ using System;
 
 public class AbilityCircle : Control
 {
+    TextureRect circleTexture;
 
+    public override void _Ready()
+    {
+        base._Ready();
+
+        circleTexture = (TextureRect)GetNode("TextureRect");
+    }
+
+    public void UpdateProgress(float progress)
+    {
+        if (!(circleTexture.Material is ShaderMaterial))
+        {
+            return;
+        }
+        ((ShaderMaterial)circleTexture.Material).SetShaderParam("currentTile", (1.0 - progress) * 8.0f);
+    }
 }

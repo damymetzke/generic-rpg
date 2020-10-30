e3917115f16919f4a3c706784533cbeea67ffe88
Damy Metzke <damy.metzke@gmail.com>
2020-10-30 09:20:37 +0100

    Implement melee ability circle for in game ui.

--- src/ui/InGameUi.cs
+++ src/ui/InGameUi.cs
@@ -4,17 +4,24 @@ using System;
 public class InGameUi : Control
 {
     HealthBar healthBar;
+    AbilityCircle abilityMelee;
 
     public override void _Ready()
     {
         base._Ready();
 
         healthBar = (HealthBar)GetNode("HealthBar");
+        abilityMelee = (AbilityCircle)GetNode("AbilityMelee");
     }
 
-    // Wrapper function
+    // Wrapper functions
     public void UpdateHealthBar(uint currentHealth, uint maxHealth)
     {
         healthBar.UpdateHealthBar(currentHealth, maxHealth);
+    }
+
+    public void UpdateAbilityMelee(float progress)
+    {
+        abilityMelee.UpdateProgress(progress);
     }
 }
